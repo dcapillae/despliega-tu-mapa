fae2d78e87808f6eae69410d6587fbf7fa254b72
dcapillae <dcapillae@gmail.com>
2019-12-29 15:41:05 +0100

    Added readme.md, license.md, and first examples

---
+++ js/site.js
@@ -0,0 +1,145 @@
+		//header map
+	  	var header_map = L.map('header_map',{scrollWheelZoom:false,zoomControl:false}).setView([36.722, -4.420], 15);
+
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org/copyright">' + 'Colaboradores de OpenStreetMap </a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas <a href="https://github.com/gravitystorm/openstreetmap-carto">OSM Carto</a> \u00a9 Randy Allan y otros colaboradores (<a href="https://creativecommons.org/licenses/by-sa/2.0/deed.es" target="blanck">CC BY-SA 2.0</a>)'
+			}).addTo(header_map);
+ 
+	  	//basic map
+		var basic_map = L.map('basic_map').setView([36.722, -4.420], 15);
+		
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org/copyright">' + 'Colaboradores de OpenStreetMap </a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas <a href="https://github.com/gravitystorm/openstreetmap-carto">OSM Carto</a> \u00a9 Randy Allan y otros colaboradores (<a href="https://creativecommons.org/licenses/by-sa/2.0/deed.es" target="blanck">CC BY-SA 2.0</a>)'
+			}).addTo(basic_map);
+
+		//multilayer basic map
+		var osmcarto = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org/copyright">' + 'Colaboradores de OpenStreetMap </a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas <a href="https://github.com/gravitystorm/openstreetmap-carto">OSM Carto</a> \u00a9 Randy Allan y otros colaboradores (<a href="https://creativecommons.org/licenses/by-sa/2.0/deed.es" target="blanck">CC BY-SA 2.0</a>)'}),
+		osmhumanitarian = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org">' +
+          'Colaboradores de OpenStreetMap</a>  (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas \u00a9 <a href="https://github.com/hotosm/HDM-CartoCSS">Humanitarian HDM CartoCSS</a> (<a href="https://github.com/hotosm/HDM-CartoCSS/blob/master/LICENCE.txt" target="blanck">CC0 1.0</a>)'
+		}),
+		pnoa = L.tileLayer.wms("http://www.ign.es/wms-inspire/pnoa-ma?SERVICE=WMS&", {
+	   layers: "OI.OrthoimageCoverage",//nombre de la capa (ver get capabilities)
+	   format: 'image/jpeg',
+	   transparent: true,
+	   version: '1.3.0',//wms version (ver get capabilities)
+	   attribution: 'Ortofotos <a href="http://www.scne.es/productos.html#OrtoPNOA" target="blanck">PNOA</a> \u00a9 <a href="http://www.ign.es" target="blanck">Instituto Geográfico Nacional de España</a> (<a href="https://creativecommons.org/licenses/by/4.0/legalcode.es" target="blanck">CC BY 4.0</a>)'
+		});
+
+		var mlb_map = L.map('mlb_map', {
+			center: [36.722, -4.420],
+			zoom: 15,
+			layers: [osmcarto]
+		});
+
+		var baseLayers = {
+		"Callejero (OSM)": osmcarto,
+		"Humanitario (OSM)": osmhumanitarian,
+		"Ortofotos (IGN)": pnoa
+		};
+
+		L.control.layers(baseLayers).addTo(mlb_map);
+
+      //map with a standard marker
+      var sm_map = L.map('sm_map').setView([36.722, -4.420], 15);
+
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org/copyright">' + 'Colaboradores de OpenStreetMap </a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas <a href="https://github.com/gravitystorm/openstreetmap-carto">OSM Carto</a> \u00a9 Randy Allan y otros colaboradores (<a href="https://creativecommons.org/licenses/by-sa/2.0/deed.es" target="blanck">CC BY-SA 2.0</a>)'
+      	}).addTo(sm_map);
+      	
+     	var marker = L.marker([36.7204,-4.4150]).addTo(sm_map);
+      	
+     	//map with a custom marker
+     	var cm_map = L.map('cm_map').setView([36.722, -4.420], 15);
+
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org/copyright">' + 'Colaboradores de OpenStreetMap </a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas <a href="https://github.com/gravitystorm/openstreetmap-carto">OSM Carto</a> \u00a9 Randy Allan y otros colaboradores (<a href="https://creativecommons.org/licenses/by-sa/2.0/deed.es" target="blanck">CC BY-SA 2.0</a>)'
+			}).addTo(cm_map);
+      	
+     	var photoIcon = L.icon({
+	  	iconUrl: 'images/photo.svg',
+	  	iconSize: [38, 95], // size of the icon
+    	});
+
+		L.marker([36.7204,-4.4150], {icon: photoIcon}).addTo(cm_map);
+		
+		//map with a custom marker and a standard popup
+		var sp_map = L.map('sp_map').setView([36.722, -4.420], 15);
+
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org/copyright">' + 'Colaboradores de OpenStreetMap </a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas <a href="https://github.com/gravitystorm/openstreetmap-carto">OSM Carto</a> \u00a9 Randy Allan y otros colaboradores (<a href="https://creativecommons.org/licenses/by-sa/2.0/deed.es" target="blanck">CC BY-SA 2.0</a>)'
+		}).addTo(sp_map);
+      	
+     	var photoIcon = L.icon({
+	  	iconUrl: 'images/photo.svg',
+	  	iconSize: [38, 95], // size of the icon
+    	});
+
+		L.marker([36.7204,-4.4150], {icon: photoIcon}).bindPopup('Ayuntamiento de Málaga').addTo(sp_map);
+		
+		//map with custom marker and custom popup
+		var cp_map = L.map('cp_map').setView([36.722, -4.420], 15);
+		
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org/copyright">' + 'Colaboradores de OpenStreetMap </a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas <a href="https://github.com/gravitystorm/openstreetmap-carto">OSM Carto</a> \u00a9 Randy Allan y otros colaboradores (<a href="https://creativecommons.org/licenses/by-sa/2.0/deed.es" target="blanck">CC BY-SA 2.0</a>)'
+		}).addTo(cp_map);
+	
+		var photoIcon = L.icon({
+	  	iconUrl: 'images/photo.svg',
+	  	iconSize: [38, 95], // size of the icon
+	  	popupAnchor: [0, -15]
+    	});
+	
+		var customPopup = "<img src='images/malaga-city-hall.jpg' alt='Ayuntamiento de Málaga' width='200px'/><br/>Ayuntamiento de Málaga. Fuente: Wikimedia Commons</a>, imagen de Daniel Capilla (CC BY-SA 4.0).";
+		 	
+		var customOptions =
+		{
+		'maxWidth': '200',
+		'className' : 'custom',
+		'autoPan' : 'false'
+		}
+		
+		L.marker([36.7204,-4.4150], {icon: photoIcon}).bindPopup(customPopup,customOptions).addTo(cp_map);
+
+		//map with a circle
+		var circle_map = L.map('circle_map').setView([36.722, -4.420], 15);
+
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org/copyright">' + 'Colaboradores de OpenStreetMap </a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas <a href="https://github.com/gravitystorm/openstreetmap-carto">OSM Carto</a> \u00a9 Randy Allan y otros colaboradores (<a href="https://creativecommons.org/licenses/by-sa/2.0/deed.es" target="blanck">CC BY-SA 2.0</a>)'
+		}).addTo(circle_map);
+
+    L.circle([36.722, -4.420], 300,{color:'blue', opacity:1, fillColor: 'yellow', fillOpacity:.5}).addTo(circle_map);
+
+		//map with a rectangle
+		var rectangle_map = L.map('rectangle_map').setView([36.722, -4.420], 15);
+
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org/copyright">' + 'Colaboradores de OpenStreetMap </a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas <a href="https://github.com/gravitystorm/openstreetmap-carto">OSM Carto</a> \u00a9 Randy Allan y otros colaboradores (<a href="https://creativecommons.org/licenses/by-sa/2.0/deed.es" target="blanck">CC BY-SA 2.0</a>)'
+		}).addTo(rectangle_map);
+
+		var bounds = [[36.725, -4.425], [36.720, -4.415]];
+
+		L.rectangle(bounds, {color: "purple", weight: 2}).addTo(rectangle_map);
+
+		//map with a polygon
+		var polygon_map = L.map('polygon_map').setView([36.722, -4.420], 15);
+
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org/copyright">' + 'Colaboradores de OpenStreetMap </a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas <a href="https://github.com/gravitystorm/openstreetmap-carto">OSM Carto</a> \u00a9 Randy Allan y otros colaboradores (<a href="https://creativecommons.org/licenses/by-sa/2.0/deed.es" target="blanck">CC BY-SA 2.0</a>)'
+		}).addTo(polygon_map);
+
+		var area = [[36.728, -4.425],[36.718, -4.425],[36.722, -4.415]];
+
+		L.polygon(area,{color:'green',opacity:.6}).addTo(polygon_map);
+		
+		//map with a polyline
+		var polyline_map = L.map('polyline_map').setView([36.722, -4.420], 15);
+
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: 'Datos \u00a9 <a href="http://www.openstreetmap.org/copyright">' + 'Colaboradores de OpenStreetMap </a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>) | Teselas <a href="https://github.com/gravitystorm/openstreetmap-carto">OSM Carto</a> \u00a9 Randy Allan y otros colaboradores (<a href="https://creativecommons.org/licenses/by-sa/2.0/deed.es" target="blanck">CC BY-SA 2.0</a>)'
+		}).addTo(polyline_map);
+
+		var toUnion = [[36.7205, -4.4130],[36.7165, -4.4260],[36.7218, -4.4250],[36.7230, -4.4240],[36.7240, -4.4220],[36.7250, -4.4220],[36.7270, -4.4240],[36.7270, -4.4250],[36.7270, -4.4270],[36.7280, -4.4300]];
+    
+	L.polyline(toUnion,{color:'red',opacity:1}).addTo(polyline_map);
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Desploy your map
+
+*Desploy your map* (in English; *Despliega tu mapa*, in Spanish) is a basic Leaflet tutorial for beginners. This is a web page which shows how to desploy a custom web map using only HTML, CSS, and the [Leaflet JavaScript](https://leafletjs.com/) library by means of examples.
+
+## Contents
+
+- Desploy a basic map (published)
+- Desploy a multilayer basic map (published)
+- Add a standard marker (published)
+- Add a custom marker (published)
+- Add a standard pop-up (published)
+- Add a custom pop-up (published)
+- Add geometric shapes (published)
+- Add a data layer (not yet published)
+- Add an data layer from an external file (not yet published)
+- Modify map properties (not yet published)
+ 
+## License
+
+Source code available under the terms of the [GNU General Public License, version 3](https://www.gnu.org/licenses/gpl-3.0-standalone.html).
+
+## Demo
+
+An online demo is available at [dcapillae.github.io/desploy-your-map](https://dcapillae.github.io/desploy-your-map).
